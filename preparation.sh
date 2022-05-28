05aaa4a4f7353971ebf5c0c4865029d6a0ed369b
pforpajri <pajri@knicles.my.id>
2022-05-28 17:50:46 +0700

    messed up

--- preparation.sh
+++ preparation.sh
@@ -1,5 +1,12 @@
 source kubernetes/config/variable.sh
 
+#ssh config for master
+ssh-keygen -t rsa
+ssh-copy-id -i ~/.ssh/id_rsa.pub ${USER}@${MASTER}
+ssh-copy-id -i ~/.ssh/id_rsa.pub ${USER}@${WORKER1}
+ssh-copy-id -i ~/.ssh/id_rsa.pub ${USER}@${WORKER1} 
+#config ssh for worker must manually configure
+
 #perintah dibawah ini dilakukan hanya di vm controller/master
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
